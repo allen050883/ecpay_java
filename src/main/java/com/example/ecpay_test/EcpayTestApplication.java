a8b01897bb73f6e466e69457b3c4118d8daadbae
allen050883 <allen050883@gmail.com>
2022-09-04 18:12:54 +0800

    new ecpay demo

---
+++ src/main/java/com/example/ecpay_test/EcpayTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecpay_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcpayTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecpay_test/EcpayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecpay_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpayTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
